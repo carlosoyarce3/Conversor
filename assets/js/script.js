d1acad7024a37129eb33cb70da74112d2e084fba
Hachi nn <carlosoyarce3@gmail.com>
2024-07-06 23:28:30 -0400

    Third Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,31 @@ const result = document.querySelector("#resultado");
 
 
 const convertir = async () =>{
-    document.querySelector("#grafico").innerHTML = "";
-    document.querySelector("#grafico").innerHTML = `<canvas id="myChart"></canvas>`;
-    const lector = await fetch("https://mindicador.cl/api/");
-    const datos = await lector.json();
-    const monto = document.querySelector("#ingreso").value;
-    const moneda = document.querySelector("#monedas").value;
-    if(datos[moneda] != undefined){
-        if(moneda == 'dolar'){
-            result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> Dolares`
-        }else if(moneda == "uf"){
-            result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> UF`
+    try {
+        document.querySelector("#grafico").innerHTML = "";
+        document.querySelector("#grafico").innerHTML = `<canvas id="myChart"></canvas>`;
+        const lector = await fetch("https://mindicador.cl/api/");
+        const datos = await lector.json();
+        const monto = document.querySelector("#ingreso").value;
+        const moneda = document.querySelector("#monedas").value;
+        if(datos[moneda] != undefined){
+            if(moneda == 'dolar'){
+                result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> Dolares`
+            }else if(moneda == "uf"){
+                result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> UF`
+            }else{
+                result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> Euros`
+            }
+            grafico(moneda);
+            
         }else{
-            result.innerHTML = `<span>${new Intl.NumberFormat('en-DE').format(monto)}</span> CLP, son <span>${new Intl.NumberFormat('en-DE').format(monto / datos[moneda].valor)}</span> Euros`
+            result.innerHTML = "Valor ingresado no es valido"
         }
-        grafico(moneda);
         
-    }else{
-        result.innerHTML = "Valor ingresado no es valido"
+    } catch (error) {
+        alert("Fallo en la api");
     }
+    
     
     
 
@@ -35,7 +41,6 @@ const grafico = async (moneda) =>{
         fechas[i] = new Date(fechas[i]);
         fechas[i] = fechas[i].toLocaleDateString("en-GB");
     }
-    
     let valores = datos.serie.slice(0,10).map(item => item.valor);
     new Chart(ctx, {
       type: 'line',
